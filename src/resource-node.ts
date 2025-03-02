471b971dc11f55bd242375bebc257f5805772058
Tobias Løfgren <tobloef@gmail.com>
2025-03-02 22:10:59 +0100

    Fix `uninitialize` optionality

--- src/resource-node.ts
+++ src/resource-node.ts
@@ -94,7 +94,7 @@ export abstract class ResourceNode<
     dependencies: Dependencies
   ): Promise<ResourceType>;
 
-  protected abstract uninitialize?(
+  protected uninitialize?(
     dependencies: Dependencies
   ): Promise<void>;
 
